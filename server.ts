3197a19c9f089ffb28317afc4fad4d1e68b80311
df2nnvctmz-crypto <df2nnvctmz@privaterelay.appleid.com>
2026-07-10 10:13:05 +0200

    chore: make server port configurable

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ import { createWorker } from "tesseract.js";
 import { FOODS } from "./src/data";
 
 const app = express();
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.use(express.json({ limit: '50mb' }));
 
